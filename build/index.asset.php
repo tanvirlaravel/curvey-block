7c50cb5dff2269aa9885d1af167c8a89fa918e1e
Tanvir <tanvirlaravel@gmail.com>
2024-09-27 12:51:50 +0600

    initail setup

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0bc53d9692df446e3306');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '89dc4249578045412ced');
