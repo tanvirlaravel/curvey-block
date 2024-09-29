549942d4b7066a1eb97bceb66de12a2258db5db7
Tanvir <tanvirlaravel@gmail.com>
2024-09-29 11:14:21 +0600

    12 - Fix the curvy block styles

--- build/blocks/curvey/index.asset.php
+++ build/blocks/curvey/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f120fe4f04b0d4f9d085');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '59b4297e4b4b9224f79f');
--- build/blocks/curvey/index.js
+++ build/blocks/curvey/index.js
@@ -19,9 +19,14 @@ function Curve() {
   return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", {
     children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("svg", {
       viewBox: "0 0 1200 120",
+      style: {
+        position: "absolute",
+        top: "0",
+        left: "0"
+      },
       children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("path", {
         style: {
-          fill: "#000"
+          fill: "#fff"
         },
         d: "M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
       })
